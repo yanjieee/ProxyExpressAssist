c944afd906e1b52134687d83fa86853c8d3c7091
yanjieee <yanjieee@gmail.com>
2014-06-12 22:24:34 +0800

    完成V1.0

--- ProxyExpressAssist/Form1.Designer.cs
+++ ProxyExpressAssist/Form1.Designer.cs
@@ -28,49 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.ForeColor = System.Drawing.Color.Red;
+            this.groupBox1.Location = new System.Drawing.Point(17, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 82);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "第二步 设置";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Red;
+            this.groupBox2.Location = new System.Drawing.Point(17, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(339, 73);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "第一步 设置";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(327, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "G:\\software\\ME\\ProxyExpress.exe";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(4, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ProxyExpress.exe 路径：";
             // 
-            // button1
+            // checkBox1
             // 
-            this.button1.Location = new System.Drawing.Point(186, 19);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(11, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "自动点击收集";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.ForeColor = System.Drawing.Color.Black;
+            this.checkBox2.Location = new System.Drawing.Point(119, 21);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(96, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "自动点击吸取";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "扫描间隔时间：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "60";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(131, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "分钟";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 72);
+            this.ClientSize = new System.Drawing.Size(375, 188);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "ProxyExpressAssist 代理快车助手";
+            this.Text = "ProxyExpressAssist 代理快车小助手 V1.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ProxyExpressAssist/Form1.cs
+++ ProxyExpressAssist/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ProxyExpressAssist
 {
@@ -19,24 +22,113 @@ namespace ProxyExpressAssist
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text == "")
+            {
+                textBox1.Focus();
+                return;
+            }
+            if (!File.Exists(textBox1.Text))
+            {
+                MessageBox.Show("文件不存在或者路径名错误！");
+                return;
+            }
+
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = false;
+            button1.Enabled = false;
+            button1.Text = "开始运行";
+
+            timer1.Interval = int.Parse(textBox2.Text) * 1000;
+            timer1.Start();
+            timer1.Tag = 0;
+            checkAndRestartPE();    //因为计时器一开始不会执行一次，所以需要先手动一次
+        }
+
+        /// <summary>
+        /// 点击菜单栏中的收集代理->启动收集
+        /// </summary>
+        /// <param name="wnd">PE的窗体句柄</param>
+        private void proxyCollect(IntPtr wnd) 
+        {
+            int collectProxy = GetMenu(wnd.ToInt32());
+            collectProxy = GetSubMenu(collectProxy, 2);
+            collectProxy = GetMenuItemID(collectProxy, 0);
+            PostMessage(wnd, BM_CLICK, collectProxy, 0);
+        }
+
+        /// <summary>
+        /// （1）点击网站吸取->选择全部（2）点击 开始吸取
+        /// </summary>
+        /// <param name="wnd">PE的窗体句柄</param>
+        private void proxyDraw(IntPtr wnd)
+        {
+            IntPtr handle = new IntPtr(0);
+            handle = FindWindowEx(wnd, handle, "TPageControl", "");
+            if (handle != IntPtr.Zero)
+            {
+                SendMessage(handle, WM_SETFOCUS, 0, 0);
+                SendMessage(handle, WM_LBUTTONDOWN, MK_LBUTTON, MAKELONG(190, 9));  //网站吸取的按钮在(190,9）这个相对坐标
+                SendMessage(handle, WM_LBUTTONUP, MK_LBUTTON, MAKELONG(190, 9));
+
+                //找到选择全部的按钮
+                IntPtr TTabSheet = new IntPtr(0);
+                TTabSheet = FindWindowEx(handle, TTabSheet, "TTabSheet", "网站吸取");
+                IntPtr TCoolBar = new IntPtr(0);
+                TCoolBar = FindWindowEx(TTabSheet, TCoolBar, "TCoolBar", "");
+                IntPtr ToolBar2 = new IntPtr(0);
+                ToolBar2 = FindWindowEx(TCoolBar, ToolBar2, "TToolBar", "");
+                IntPtr TCheckBox = new IntPtr(0);
+                TCheckBox = FindWindowEx(ToolBar2, TCheckBox, "TCheckBox", "选择全部");
+                SendMessage(TCheckBox, WM_CLICK, 0, 0);
+                SendMessage(ToolBar2, WM_LBUTTONDOWN, MK_LBUTTON, MAKELONG(10, 10));  //开始的按钮在ToolBar2的(10,10）这个相对坐标
+                SendMessage(ToolBar2, WM_LBUTTONUP, MK_LBUTTON, MAKELONG(10, 10));
+
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            checkAndRestartPE();
+        } 
+
+        /// <summary>
+        /// 判断并重启PE
+        /// </summary>
+        private void checkAndRestartPE()
+        {
             IntPtr wnd = new IntPtr(0);
             wnd = FindWindow("Tfrm_MainForm", "ProxyExpress 代理快车");
-            if (wnd != IntPtr.Zero)
-            {
-                IntPtr pageControl = new IntPtr(0);
-                pageControl = FindWindowEx(wnd, pageControl, "TPageControl", "");
-                if (pageControl != IntPtr.Zero)
-                {
-                    int a = SendMessage(pageControl, WM_SETFOCUS, 0, 0);
-                    a = SendMessage(pageControl, WM_LBUTTONDOWN, MK_LBUTTON, MAKELONG(190, 9));
-                    a = SendMessage(pageControl, WM_LBUTTONUP, MK_LBUTTON, MAKELONG(190, 9));
-//                     NMHDR nmhdr = new NMHDR();
-//                     nmhdr.hwndFrom = (UInt32)pageControl;
-//                     int a = SendMessage(pageControl, WM_NOTIFY, new IntPtr(TCN_SELCHANGE), nmhdr);
 
+            if (wnd == IntPtr.Zero)
+            {
+                //已经停止
+                Process proxyProcess = new Process();
+                proxyProcess.StartInfo.FileName = textBox1.Text;
+                proxyProcess.Start();
+                button1.Text = "正在重启";
+                while (wnd == IntPtr.Zero)
+                {
+                    Thread.Sleep(1000);
+                    wnd = FindWindow("Tfrm_MainForm", "ProxyExpress 代理快车");
+                }
+
+                int count = (int)timer1.Tag;
+                count++;
+                timer1.Tag = count;
+                button1.Text = "已重启" + count.ToString() + "次";
+
+                if (checkBox1.Checked)
+                {
+                    proxyCollect(wnd);
+                }
+                if (checkBox2.Checked)
+                {
+                    proxyDraw(wnd);
                 }
             }
         }
+
 
         const int WM_GETTEXT = 0x000D;
         const int WM_SETTEXT = 0x000C;
@@ -47,13 +139,8 @@ namespace ProxyExpressAssist
         const int WM_NOTIFY = 0x004E;
         const int MK_LBUTTON = 0x0001;
         const int WM_SETFOCUS = 0x0007;
+        const int BM_CLICK = 0x111;
 
-        struct NMHDR
-        {
-            public UInt32 hwndFrom;
-            public UInt32 idFrom;
-            public UInt32 code;         // NM_ code
-        }
 
         [DllImport("user32.dll", EntryPoint = "FindWindow")]
         private extern static IntPtr FindWindow(string lpClassName, string lpWindowName);
@@ -67,14 +154,28 @@ namespace ProxyExpressAssist
         [DllImport("user32.dll", EntryPoint = "SendMessage")]
         private static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
 
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hwnd, int wMsg, uint wParam, uint lParam);
+
+        //C#中引用PostMessage函数的方法：向指定hwnd的窗体或者控件发送命令消息
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr PostMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        //C#中引用GetMenu函数的方法：得到指定窗口的主菜单
+        [DllImport("user32.dll", EntryPoint = "GetMenu")]
+        private static extern int GetMenu(int hwnd);
+
+        //C#中引用GetSubMenu函数的方法：得到指定主菜单的子菜单
+        [DllImport("user32.dll", EntryPoint = "GetSubMenu")]
+        private static extern int GetSubMenu(int hMenu, int nPos);
+
+        //C#中引用GetMenuItemID函数的方法：得到指定子菜单的ID号
+        [DllImport("user32.dll", EntryPoint = "GetMenuItemID")]
+        private static extern int GetMenuItemID(int hMenu, int nPos);
+
         private static int MAKELONG(ushort x, ushort y) 
         {
             return (x + (y<<16)); //low order WORD 是指标的x位置； high order WORD是y位置. 
-        } 
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
 
         
